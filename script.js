1b14241db5f8745ff7409c6a0dcdefe35620fd5c
Mesrop <mesropavetisyan94@yandex.ru>
2020-02-20 23:59:55 +0400

    FormTest

---
+++ script.js
@@ -0,0 +1,17 @@
+let FormSwitches = document.querySelectorAll("[data-form]");
+
+for (let i = 0; i < FormSwitches.length; i++) {
+    FormSwitches[i].addEventListener("click", switchForm)
+}
+
+function switchForm(ev) {
+    let forms = document.querySelectorAll(".forms-inputs>div");
+    for (let i = 0; i < forms.length; i++) {
+        forms[i].classList.add("hidden");
+    }
+    let formClass = ev.target.getAttribute("data-form");
+    let form = document.querySelector("." + formClass);
+    if (form) {
+        form.classList.remove("hidden");
+    }
+}
