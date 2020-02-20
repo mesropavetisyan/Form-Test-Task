653ee1f9d2d0ed2dbc1d7af6f64a0602b81ec1ae
Mesrop <mesropavetisyan94@yandex.ru>
2020-02-21 01:45:19 +0400

    full name validate

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let FormSwitches = document.querySelectorAll("[data-form]");
-
+let validate = {
+    fullname: function (ev) {
+        let v = this.value;
+        if (v.length == 0) {
+            return;
+        }
+        v = v[v.length - 1].codePointAt(0);
+        if (!((v >= 65 && v <= 90) ||
+            (v >= 97 && v <= 122) ||
+            (v == 32 || v == 39 || v == 45))) {
+            this.value = this.value.slice(0, -1);
+        }
+    }
+};
 for (let i = 0; i < FormSwitches.length; i++) {
     FormSwitches[i].addEventListener("click", switchForm)
 }
@@ -15,3 +28,7 @@ function switchForm(ev) {
         form.classList.remove("hidden");
     }
 }
+
+let fn = document.querySelectorAll('[name="full-name"]');
+fn[0].addEventListener("input",validate.fullname);
+fn[1].addEventListener("input",validate.fullname);
