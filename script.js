5276a2febcec86ffd07eadce54e8e70cf9a02d70
Mesrop <mesropavetisyan94@yandex.ru>
2020-02-22 20:18:34 +0400

    btn click next page

--- script.js
+++ script.js
@@ -1,17 +1,54 @@
-let FormSwitches = document.querySelectorAll("[data-form]");
+let formSwitches = document.querySelectorAll("[data-form]");
 
-for (let i = 0; i <FormSwitches.length ; i++) {
-    FormSwitches[i].addEventListener("click", switchForm)
+for (let i = 0; i <formSwitches.length ; i++) {
+    formSwitches[i].addEventListener("click", switchForm)
 }
 
-function switchForm(ev) {
-    let forms= document.querySelectorAll(".forms-input>div");
+function switchForm(ev,f) {
+    let forms= document.querySelectorAll(".forms-inputs>div");
     for (let i = 0; i < forms.length; i++) {
         forms[i].classList.add("hidden");
     }
-    let formClass=ev.target.getAttribute("data-form");
-    let form = document.querySelector("." + formClass);
+    let form = null;
+    if(f){
+        form = f;
+    }else{
+        let formClass=ev.target.getAttribute("data-form");
+        form = document.querySelector("." + formClass);
+    }
     if(form){
         form.classList.remove("hidden");
     }
 }
+
+let btn = document.querySelector(".form-btn");
+btn.addEventListener("click", function () {
+    let form = this.closest(".form-desc");
+    let fillInp = form.querySelectorAll(".inp-fill");
+    for (let i = 0; i < fillInp.length; i++) {
+        if(fillInp[i].value.length == "") {
+            fillInp[i].classList.add(".no-fill")
+        }else {
+            fillInp[i].classList.remove(".no-fill");
+            let messageInvalid = fillInp[i].closest(".invalid-field").querySelector("invalid-message");
+            if(messageInvalid) {
+                messageInvalid.classList.remove("show");
+            }
+        }
+    }
+    let invalid = document.querySelector('.no-fill');
+    if (invalid) {
+        invalid.focus();
+        let messageInvalid = invalid.closest('.invalid-field').querySelector('.invalid-message');
+        if (messageInvalid) {
+            messageInvalid.classList.add('show');
+        }
+    } else {
+        switchForm(null, form.nextElementSibling);
+    }
+});
+
+
+
+
+
